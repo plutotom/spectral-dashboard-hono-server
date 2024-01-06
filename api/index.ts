bae5c4137d6a878b9f71c628791e622b5c0751a0
Isaiah Proctor <plutotom@live.com>
2024-01-06 14:49:57 -0600

    done with hono i guess

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { env } from "hono/adapter";
 const { Client } = require("@notionhq/client");
+import { prettyJSON } from "hono/pretty-json";
+import { cors } from "hono/cors";
 
 export const config = {
   runtime: "edge",
@@ -9,6 +11,8 @@ export const config = {
 };
 
 const app = new Hono().basePath("/api");
+app.use("*", prettyJSON()); // With options: prettyJSON({ space: 4 })
+app.use("*", cors());
 
 app.get("/", (c) => {
   return c.json({ message: "Hello Hono!" });
@@ -24,8 +28,19 @@ app.get("/notion/users", async (c) => {
 
   // Getting a user
   const listUsersResponse = await notion.users.list({});
-  return c.json(listUsersResponse);
+  // return plain json
+  c.header("Content-Type", "application/json");
+  return c.json({ users: listUsersResponse, ok: true });
 });
+
+// api.post('/posts', async (c) => {
+//   const param = await c.req.json()
+//   const newPost = await model.createPost(c.env.BLOG_EXAMPLE, param as model.Param)
+//   if (!newPost) {
+//     return c.json({ error: 'Can not create new post', ok: false }, 422)
+//   }
+//   return c.json({ post: newPost, ok: true }, 201)
+// })
 
 app.notFound((c) => {
   return c.text("Custom 404 Message from Isaiah", 404);
