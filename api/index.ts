350fee0824b2b6fddaa09857907c6d56fa2a2d0c
Isaiah Proctor <plutotom@live.com>
2024-01-06 12:51:19 -0600

    added notion route

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,34 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { env } from "hono/adapter";
+const { Client } = require("@notionhq/client");
 
 export const config = {
-  runtime: 'edge'
-}
+  runtime: "edge",
+  port: 5005,
+};
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+app.get("/", (c) => {
+  return c.json({ message: "Hello Hono!" });
+});
 
-export default handle(app)
+app.get("/notion/users", async (c) => {
+  const url = "notion.so/api/notion/users";
+  // Initializing a client
+  const { NOTION_SECRET } = env<{ NOTION_SECRET: string }>(c);
+  const notion = new Client({
+    auth: NOTION_SECRET,
+  });
+
+  // Getting a user
+  const listUsersResponse = await notion.users.list({});
+  return c.json(listUsersResponse);
+});
+
+app.notFound((c) => {
+  return c.text("Custom 404 Message from Isaiah", 404);
+});
+
+export default handle(app);
